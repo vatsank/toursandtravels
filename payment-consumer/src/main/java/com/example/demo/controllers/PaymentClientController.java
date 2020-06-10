92b141546a96a7d9b5208fbbef71c5c6fdbd4d6e
Srivatsan <'vatsank@gmail.com'>
2020-06-10 12:22:26 +0530

    consumer Service Added

---
+++ payment-consumer/src/main/java/com/example/demo/controllers/PaymentClientController.java
@@ -0,0 +1,27 @@
+package com.example.demo.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class PaymentClientController {
+
+	
+	@Autowired
+	private RestTemplate template;
+	
+	@GetMapping(path = "/client")
+	public String fetchPayments() {
+		
+		// The following code would have been used with Service Registry and Discovery
+		
+	//String resp2 = template.getForObject("http://localhost:2020/payment", String.class)
+		
+		// Since SRD is available we use a logical name
+	  String resp = template.getForObject("http://PAYMENT-SERVICE/payment", String.class);
+	
+	   return resp;
+	}
+}
