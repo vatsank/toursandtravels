92ce03fc93bb7fb81e0c2123184e00db33dada95
Srivatsan <'vatsank@gmail.com'>
2020-06-10 13:01:05 +0530

    Update

--- tourist-guide-service/src/main/java/com/example/demo/TouristGuideServiceApplication.java
+++ tourist-guide-service/src/main/java/com/example/demo/TouristGuideServiceApplication.java
@@ -2,11 +2,13 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 import com.example.demo.domains.TouristGuide;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class TouristGuideServiceApplication {
 
 	public static void main(String[] args) {
