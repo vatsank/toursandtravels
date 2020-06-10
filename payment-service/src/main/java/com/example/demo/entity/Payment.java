6c527b2b05e2558a196db2d63413a223dad2a166
Srivatsan <'vatsank@gmail.com'>
2020-06-10 11:39:16 +0530

    Producer Service Added

---
+++ payment-service/src/main/java/com/example/demo/entity/Payment.java
@@ -0,0 +1,18 @@
+package com.example.demo.entity;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Data
+public class Payment {
+
+	int txnId;
+	String customerName;
+	double amount;
+}
