f17fac7293a945303738f01b018edfc18d5157ff
Srivatsan <'vatsank@gmail.com'>
2020-06-11 14:46:26 +0530

    Update

--- ImageService/src/main/java/com/example/demo/controllers/ImageController.java
+++ ImageService/src/main/java/com/example/demo/controllers/ImageController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,9 +12,13 @@ public class ImageController {
 	@Autowired
 	private Image image;
 	
+	@Value("${eureka.instance.instance-id}")
+	private String instanceId;
+	
 	@GetMapping(path = "/images/{id}")
 	public Image getImage(@PathVariable("id") int id) {
 		
+		image.setDescription(instanceId);
 		return image;
 	}
 }
