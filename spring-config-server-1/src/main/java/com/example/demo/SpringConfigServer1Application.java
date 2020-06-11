8388f6521cd579bce093ec203db2951ce3ea72ec
Srivatsan <'vatsank@gmail.com'>
2020-06-11 13:06:23 +0530

    Config Server Admin Server Examples Added

---
+++ spring-config-server-1/src/main/java/com/example/demo/SpringConfigServer1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConfigServer1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringConfigServer1Application.class, args);
+	}
+
+}
