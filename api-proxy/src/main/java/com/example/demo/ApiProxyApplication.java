49f3a4440f86b22c96437f1fe4a27837d81d48ed
Srivatsan <'vatsank@gmail.com'>
2020-06-12 21:17:07 +0530

    Fixed Indentation Issue in api-proxy project

--- api-proxy/src/main/java/com/example/demo/ApiProxyApplication.java
+++ api-proxy/src/main/java/com/example/demo/ApiProxyApplication.java
@@ -3,10 +3,11 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
 @EnableZuulProxy
 public class ApiProxyApplication {
 
