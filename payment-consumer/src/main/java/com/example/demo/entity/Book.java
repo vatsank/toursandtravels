43ce319c35b7f26645e5eaab88b038b8db58b14e
srivatsan <vatsan_k@hotmail.com>
2021-10-08 10:11:10 +0530

    adding book class and updating employee with toString method

---
+++ payment-consumer/src/main/java/com/example/demo/entity/Book.java
@@ -0,0 +1,25 @@
+package com.example.demo.entity;
+
+public class Book {
+
+	private int bookNumber;
+	private String bookName;
+	public Book() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public int getBookNumber() {
+		return bookNumber;
+	}
+	public void setBookNumber(int bookNumber) {
+		this.bookNumber = bookNumber;
+	}
+	public String getBookName() {
+		return bookName;
+	}
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+	
+	
+}
